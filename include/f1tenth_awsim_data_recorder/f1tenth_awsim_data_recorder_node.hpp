1ae90b59033d6e0a72c70652ef0663e63afac66a
Szymon Kwiatkowski <szymon.z.kwiatkowski@gmail.com>
2024-06-06 09:41:02 +0200

    feat: changes to require car state

--- include/f1tenth_awsim_data_recorder/f1tenth_awsim_data_recorder_node.hpp
+++ include/f1tenth_awsim_data_recorder/f1tenth_awsim_data_recorder_node.hpp
@@ -24,7 +24,10 @@
 #include "message_filters/sync_policies/approximate_time.h"
 #include "autoware_auto_control_msgs/msg/ackermann_control_command.hpp"
 #include "autoware_auto_planning_msgs/msg/trajectory.hpp"
+#include "autoware_auto_vehicle_msgs/msg/gear_command.hpp"
+#include "autoware_adapi_v1_msgs/msg/operation_mode_state.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+
 
 #include "f1tenth_awsim_data_recorder/f1tenth_awsim_data_recorder.hpp"
 
@@ -52,13 +55,13 @@ public:
 
 private:
   F1tenthAwsimDataRecorderPtr f1tenth_awsim_data_recorder_{nullptr};
-  
-  std::shared_ptr<message_filters::TimeSynchronizer<
-  geometry_msgs::msg::PoseStamped,
-  autoware_auto_planning_msgs::msg::Trajectory>> _time_synchronizer;
 
+  bool recording_active = false;
   std::shared_ptr<LatestSynchronizer> _synchronizer;
+  std::shared_ptr<LatestSynchronizer> _carStateSynchronizer;
 
+  message_filters::Subscriber<autoware_adapi_v1_msgs::msg::OperationModeState> mode_state_sub_;
+  message_filters::Subscriber<autoware_auto_vehicle_msgs::msg::GearCommand> gear_command_sub_;
   message_filters::Subscriber<autoware_auto_control_msgs::msg::AckermannControlCommand> ackerman_sub_;
   message_filters::Subscriber<geometry_msgs::msg::PoseStamped> ground_truth_topic_sub_;
   message_filters::Subscriber<autoware_auto_planning_msgs::msg::Trajectory> trajectory_sub_;
@@ -67,6 +70,11 @@ private:
     const autoware_auto_control_msgs::msg::AckermannControlCommand::ConstSharedPtr & ackermann,
     const geometry_msgs::msg::PoseStamped::ConstSharedPtr & ground_truth,
     const autoware_auto_planning_msgs::msg::Trajectory::ConstSharedPtr & trajectory
+  );
+
+  void VehicleStateCallback(
+    const autoware_adapi_v1_msgs::msg::OperationModeState::ConstSharedPtr & operation_mode,
+    const autoware_auto_vehicle_msgs::msg::GearCommand::ConstSharedPtr & gear_cmd
   );
 };
 }  // namespace f1tenth_awsim_data_recorder
--- src/f1tenth_awsim_data_recorder_node.cpp
+++ src/f1tenth_awsim_data_recorder_node.cpp
@@ -19,6 +19,8 @@
 using std::placeholders::_1;
 using std::placeholders::_2;
 using std::placeholders::_3;
+using std::placeholders::_4;
+using std::placeholders::_5;
 using namespace std::chrono_literals;
 
 namespace f1tenth_awsim_data_recorder
@@ -33,12 +35,16 @@ F1tenthAwsimDataRecorderNode::F1tenthAwsimDataRecorderNode(const rclcpp::NodeOpt
   std::string ackermann_topic = declare_parameter("ackermann_topic", "/control/command/control_cmd");
   std::string ground_truth_topic = declare_parameter("ground_truth_topic", "/localization/cartographer/pose");
   std::string trajectory_topic = declare_parameter("trajectory_topic", "/planning/racing_planner/trajectory");
+  std::string gear_topic = declare_parameter("gear_topic", "/external/selected/gear_cmd");
+  std::string operation_mode_state_topic = declare_parameter("operation_mode_state_topic", "/system/operation_mode/state");
 
   RCLCPP_INFO(this->get_logger(), 
-              "Ackermann topic: %s, Localization topic: %s, Trajectory topic: %s", 
+              "Ackermann topic: %s, Localization topic: %s, Trajectory topic: %s, Gear topic: %s, State topic: %s", 
               ackermann_topic.c_str(), 
               ground_truth_topic.c_str(), 
-              trajectory_topic.c_str());
+              trajectory_topic.c_str(),
+              gear_topic.c_str(),
+              operation_mode_state_topic.c_str());
 
   f1tenth_awsim_data_recorder_ = std::make_unique<f1tenth_awsim_data_recorder::F1tenthAwsimDataRecorder>();
   f1tenth_awsim_data_recorder_->SetMaxPoints(max_points_count);
@@ -47,6 +53,9 @@ F1tenthAwsimDataRecorderNode::F1tenthAwsimDataRecorderNode(const rclcpp::NodeOpt
   ground_truth_topic_sub_.subscribe(this, ground_truth_topic, qos.get_rmw_qos_profile());
   trajectory_sub_.subscribe(this, trajectory_topic, qos.get_rmw_qos_profile());
 
+  gear_command_sub_.subscribe(this, gear_topic, qos.get_rmw_qos_profile());
+  mode_state_sub_.subscribe(this, operation_mode_state_topic, qos.get_rmw_qos_profile());
+
   _synchronizer = std::make_shared<LatestSynchronizer>(
     lastest_policy(this->get_clock()),
     ackerman_sub_,
@@ -54,14 +63,24 @@ F1tenthAwsimDataRecorderNode::F1tenthAwsimDataRecorderNode(const rclcpp::NodeOpt
     trajectory_sub_
   );
 
-  std::tuple<double, double, double> config = std::tuple<double, double, double>(
-    1, 1, 0
+  _carStateSynchronizer = std::make_shared<LatestSynchronizer>(
+    lastest_policy(this->get_clock()),
+    mode_state_sub_,
+    gear_command_sub_
   );
 
-  _synchronizer->setRateConfig( config );
+  // std::tuple<double, double, double> config = std::tuple<double, double, double>(
+  //   1, 1, 0
+  // );
+
+  // _synchronizer->setRateConfig( config );
 
   _synchronizer->registerCallback(
     std::bind(&F1tenthAwsimDataRecorderNode::SynchronizerCallback, this, _1, _2, _3)
+  );
+
+  _carStateSynchronizer->registerCallback(
+    std::bind(&F1tenthAwsimDataRecorderNode::VehicleStateCallback, this, _1, _2)
   );
 
   RCLCPP_INFO(this->get_logger(), 
@@ -75,8 +94,29 @@ void F1tenthAwsimDataRecorderNode::SynchronizerCallback(
   )
 {
   // RCLCPP_INFO(this->get_logger(), "Trajectory count: %lu", trajectory->points.size());
+  if (!recording_active)
+  {
+    return;
+  }
+  
   f1tenth_awsim_data_recorder_->SaveToCsv(ackermann, ground_truth, trajectory);
 }
+
+void F1tenthAwsimDataRecorderNode::VehicleStateCallback(
+    const autoware_adapi_v1_msgs::msg::OperationModeState::ConstSharedPtr & operation_mode,
+    const autoware_auto_vehicle_msgs::msg::GearCommand::ConstSharedPtr & gear_cmd
+  )
+  {
+    recording_active = (gear_cmd->command == 2) && (operation_mode->mode == 2);
+    if (recording_active)
+    {
+      RCLCPP_INFO(this->get_logger(), "Recording active");
+    }
+    else
+    {
+      RCLCPP_INFO(this->get_logger(), "Recording not active");
+    }
+  }
 
 }  // namespace f1tenth_awsim_data_recorder
 
